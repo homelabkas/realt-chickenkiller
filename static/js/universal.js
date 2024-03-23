280b3c39cdbefb59a321202ea48b9346e7d674d0
homelabkas <162630546+homelabkas@users.noreply.github.com>
2024-03-23 11:18:20 +0100

    new center default view

--- static/js/universal.js
+++ static/js/universal.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([32, -83], 4);
+var map = L.map('map').setView([32, -43], 4);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
